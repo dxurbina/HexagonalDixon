c028d3ebaa9ef2f24ce9391b73b4f36edca89396
Dixon Urbina <dixon.u@winterinternation.io>
2024-04-29 16:05:16 -0600

    add folder out shared

---
+++ Hexagonal/Shared/Domain/Ports/Out/IDbCrudAdapter.cs
@@ -0,0 +1,12 @@
+﻿namespace Hexagonal.Shared.Domain.Ports.Out
+{
+    public interface IDbCrudAdapter<TModel>
+    {
+        Task<List<TModel>> FindAll();
+        Task<TModel?> FindById(int id);
+        Task<TModel> Create(TModel model);
+        Task<TModel> Update(int id, TModel model);
+        Task<int> Delete(int id);
+    }
+}
+
